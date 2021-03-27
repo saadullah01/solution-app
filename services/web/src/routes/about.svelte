7e38aa6df88ec792aacdcbc751a92f40f08e4877
saadullah01 <saadullah1912@gmail.com>
2021-03-28 01:46:03 +0500

    ch-9/lesson-5/1

--- services/web/src/routes/about.svelte
+++ services/web/src/routes/about.svelte
@@ -1,6 +1,15 @@
 <script>
+  import ContactCard from "../components/contact-card.svelte";
   import FeatureToggle from "../components/feature-toggle.svelte";
   import { FEATURE_TOGGLES } from "../feature-toggles";
+
+  const erinLindford = {
+    name: "Erin Lindford",
+    avatarSrc: "https://randomuser.me/api/portraits/women/17.jpg",
+    job: "Customer Support",
+    email: " erinlindford@example.com ",
+    phone: "(555) 765-4321"
+  }
 </script>
 
 <svelte:head>
@@ -14,3 +23,5 @@
 <FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
   <p>Some people see this, others don't!</p>
 </FeatureToggle>
+
+<ContactCard {...erinLindford} />
