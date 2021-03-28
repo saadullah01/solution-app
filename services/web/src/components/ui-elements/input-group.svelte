44248164e47243fe685a1f2db1f1c3932a86600d
saadullah01 <saadullah1912@gmail.com>
2021-03-28 19:13:06 +0500

    ch-11/lesson-4/1

--- services/web/src/components/ui-elements/input-group.svelte
+++ services/web/src/components/ui-elements/input-group.svelte
@@ -4,6 +4,9 @@
     export let id;
     export let placeholder;
     export let value;
+
+    export let errorMessage;
+    export let hasError;
 </script>
   
 <label for={id} class="mt-4 block text-sm font-medium leading-5 text-gray-700">
@@ -16,4 +19,16 @@
     {:else if elementType === "textarea"}
       <textarea bind:value rows="10" {placeholder} class="block w-full"></textarea>
     {/if}
+
+    {#if hasError}
+      <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+        <svg class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+          <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+        </svg>
+      </div>
+    {/if}
 </div>
+
+{#if hasError}
+  <p class="mt-2 text-sm text-red-600">{errorMessage}</p>
+{/if}
