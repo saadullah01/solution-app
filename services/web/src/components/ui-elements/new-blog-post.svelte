5ebbce2711cf6aa13688b079af1247b07d6d8d21
saadullah01 <saadullah1912@gmail.com>
2021-03-28 18:55:28 +0500

    ch-11/lesson-3/3

---
+++ services/web/src/components/ui-elements/new-blog-post.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Button from './ui-elements/button.svelte';
+    import InputGroup from './ui-elements/input-group.svelte';
+  
+    let title;
+    let content;
+  
+    $: isPublishDisabled = !(title && content);
+</script>
+  
+<h1>Add new blog post</h1>
+<div class="mt-6">
+    <form>
+      <InputGroup elementType="input" label="Title" id="title" placeholder="Untitled" bind:value={title} />
+      <InputGroup elementType="textarea" label="Content" id="content" placeholder="Your content..." bind:value={content} />
+      <Button disabled={isPublishDisabled}>Publish</Button>
+    </form>
+</div>
