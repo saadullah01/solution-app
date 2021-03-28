18960ff7b1d4e25d296f5cbb607700e98b831568
saadullah01 <saadullah1912@gmail.com>
2021-03-28 18:56:56 +0500

    ch-11/lesson-3/4

--- services/web/src/routes/admin/index.svelte
+++ services/web/src/routes/admin/index.svelte
@@ -1,16 +1,16 @@
 <script>
-    import { goto } from '@sapper/app';
-    import { onMount } from "svelte";
-    import { currentUser } from "../../stores/user";
-  
-    onMount(() => {
-      if (!$currentUser) {
-        goto("/auth/login?redirect=admin")
-      }
-    });
+  import { goto } from '@sapper/app';
+  import { onMount } from "svelte";
+  import { currentUser } from "../../stores/user";
+  import NewBlogPost from "../../components/new-blog-post.svelte";
+
+  onMount(() => {
+    if (!$currentUser) {
+      goto("/auth/login?redirect=admin")
+    }
+  });
 </script>
 
 {#if $currentUser}
-  <h1>Admin dashboard</h1>
-  <p>Only authenticated users have access to this.</p>
+  <NewBlogPost />
 {/if}
