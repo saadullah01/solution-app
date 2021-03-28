77aa6c07daa69e59e3f1e8a9591f6158081a7752
saadullah01 <saadullah1912@gmail.com>
2021-03-28 14:51:07 +0500

    ch-10/lesson-5/1

--- services/web/src/routes/admin/index.svelte
+++ services/web/src/routes/admin/index.svelte
@@ -1,2 +1,16 @@
-<h1>Admin dashboard</h1>
-<p>Only authenticated users have access to this.</p>
+<script>
+    import { goto } from '@sapper/app';
+    import { onMount } from "svelte";
+    import { currentUser } from "../../stores/user";
+  
+    onMount(() => {
+      if (!$currentUser) {
+        goto("/auth/login")
+      }
+    });
+</script>
+
+{#if $currentUser}
+  <h1>Admin dashboard</h1>
+  <p>Only authenticated users have access to this.</p>
+{/if}
