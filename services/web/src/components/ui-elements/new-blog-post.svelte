c39ec9eac93185c77e68b30fe8b046a052a62263
saadullah01 <saadullah1912@gmail.com>
2021-03-28 19:15:20 +0500

    ch-11/lesson-4/2

--- services/web/src/components/ui-elements/new-blog-post.svelte
+++ services/web/src/components/ui-elements/new-blog-post.svelte
@@ -11,8 +11,24 @@
 <h1>Add new blog post</h1>
 <div class="mt-6">
     <form>
-      <InputGroup elementType="input" label="Title" id="title" placeholder="Untitled" bind:value={title} />
-      <InputGroup elementType="textarea" label="Content" id="content" placeholder="Your content..." bind:value={content} />
+      <InputGroup 
+      elementType="input" 
+      label="Title" 
+      errorMessage="Please provide a title."
+      hasError={!title}
+      id="title" 
+      placeholder="Untitled" 
+      bind:value={title} />
+
+      <InputGroup 
+      elementType="textarea" 
+      label="Content" 
+      errorMessage="Please provide content."
+      hasError={!content}
+      id="content" 
+      placeholder="Your content..." 
+      bind:value={content} />
+      
       <Button disabled={isPublishDisabled}>Publish</Button>
     </form>
 </div>
