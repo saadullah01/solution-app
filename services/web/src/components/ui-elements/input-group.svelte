a1aa23129a5504e46755a1e630e94930fba231dd
saadullah01 <saadullah1912@gmail.com>
2021-03-28 18:52:43 +0500

    ch-11/lesson-3/1

---
+++ services/web/src/components/ui-elements/input-group.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let elementType;
+    export let label;
+    export let id;
+    export let placeholder;
+    export let value;
+</script>
+  
+<label for={id} class="mt-4 block text-sm font-medium leading-5 text-gray-700">
+    {label}
+</label>
+  
+<div class="mt-1 relative rounded-md shadow-sm">
+    {#if elementType === "input"}
+      <input bind:value {id} {placeholder} class="block w-full" />
+    {:else if elementType === "textarea"}
+      <textarea bind:value rows="10" {placeholder} class="block w-full"></textarea>
+    {/if}
+</div>
