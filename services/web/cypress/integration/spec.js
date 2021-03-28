35b9dedcb62c77a530862b0143bc3e0b935fc424
saadullah01 <saadullah1912@gmail.com>
2021-03-28 23:14:11 +0500

    update

--- services/web/cypress/integration/spec.js
+++ services/web/cypress/integration/spec.js
@@ -1,44 +1,40 @@
-import posts from "../../src/routes/blog/_posts";
-
 describe("Sapper template app", () => {
-    beforeEach(() => {
-      cy.visit("/");
-    });
-  
-    it("has the correct <h1>", () => {
-      cy.contains("h1", "Great success!");
-    });
-  
-    it("has the correct heading", () => {
-      // https://testing-library.com/docs/guide-which-query
-      cy.findByRole("heading").should("contain", "Great success!");
-    });
+  beforeEach(() => {
+    cy.visit("/");
+  });
 
-    it("navigates to /about", () => {
-      cy.get("nav a").contains("about").click();
-      cy.url().should("include", "/about");
-    });
-  
-    it("navigates to /blog", () => {
-      cy.get("nav a").contains("blog").click();
-      cy.url().should("include", "/blog");
-    });
+  it("has the correct <h1>", () => {
+    cy.contains("h1", "Great success!");
+  });
+
+  it("has the correct heading", () => {
+    // https://testing-library.com/docs/guide-which-query
+    cy.findByRole("heading").should("contain", "Great success!");
+  });
+
+  it("navigates to /about", () => {
+    cy.get("nav a").contains("about").click();
+    cy.url().should("include", "/about");
+  });
+
+  it("navigates to /blog", () => {
+    cy.get("nav a").contains("blog").click();
+    cy.url().should("include", "/blog");
+  });
 });
 
 describe('Blog posts', () => {
-    beforeEach(() => {
-     cy.visit('/blog')
-    });
-  
-    it("has the correct <h1>", () => {
-      cy.contains("h1", "Recent posts");
-    });
-  
-    posts.forEach(post => {
-      it(`lists the "${post.title}" blog post`, () => {
-        cy
-          .contains('[data-cy=blog-posts-list] li a', post.title)
-          .should('have.attr', 'href', `blog/${post.slug}`)
-      })
-    });
+  beforeEach(() => {
+    cy.visit('/blog')
   });
+
+  it("has the correct <h1>", () => {
+    cy.contains("h1", "Recent posts");
+  });
+
+  it("displays blog posts", () => {
+    cy.get("[data-cy=blog-posts-list] li").should(
+      "not.have.length", 0
+    );
+  });
+});
