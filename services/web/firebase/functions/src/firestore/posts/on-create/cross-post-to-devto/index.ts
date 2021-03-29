f7fafb48e1b3eb504e9fdc7dd498236307f916c0
saadullah01 <saadullah1912@gmail.com>
2021-03-29 15:04:53 +0500

    update

--- services/web/firebase/functions/src/firestore/posts/on-create/cross-post-to-devto/index.ts
+++ services/web/firebase/functions/src/firestore/posts/on-create/cross-post-to-devto/index.ts
@@ -1,9 +1,43 @@
 import * as functions from "firebase-functions";
+import got from "got";
 
-export default functions.firestore
-  .document('posts/{slug}')
-  .onCreate((snapshot: functions.firestore.DocumentSnapshot,
-context: functions.EventContext) => {
-  console.log(`New post created with slug: ${context.params.slug}. Post: ${JSON.stringify(snapshot.data())}`)
+interface IBlogPost {
+  title: string;
+  content: string;
+}
+
+const publishToDevto = (blogPost: IBlogPost) => got.post("https://dev.to/api/articles", {
+  headers: {
+    "api-key": functions.config().crossposttodevto.apikey
+  },
+  json: {
+    article: {
+      title: blogPost.title,
+      body_markdown: blogPost.content,
+      published: false
+    }
+  }
 });
 
+export default functions.firestore
+  .document("posts/{slug}")
+  .onCreate(
+    async (
+      snapshot: functions.firestore.DocumentSnapshot,
+      context: functions.EventContext
+    ) => {
+      const blogPost = snapshot.data() as IBlogPost || {};
+      console.log(
+        `Posting to dev.to... Slug: ${
+        context.params.slug
+        }. Post: ${JSON.stringify(blogPost)}`
+      );
+
+      try {
+        await publishToDevto(blogPost);
+        console.log("Blog post successfully posted to dev.to.");
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  );
